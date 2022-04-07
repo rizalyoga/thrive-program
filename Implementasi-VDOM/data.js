b304f9f0b342b6fc91437925cf5e095fa5f4ee40
rizalyoga <ach.rizal.yogaswara@gmail.com>
2022-04-07 09:29:50 +0700

    thrive day-1 VDOM

---
+++ Implementasi-VDOM/data.js
@@ -0,0 +1,20 @@
+const dataCharacter = [
+  {
+    imageProfile: "https://upload.wikimedia.org/wikipedia/en/b/bd/Doraemon_character.png",
+    name: "Doraemom",
+    city: "Toyama",
+    age: "40",
+  },
+  {
+    imageProfile: "https://pbs.twimg.com/media/DyXR3quXQAUBo81.jpg",
+    name: "Itachi",
+    city: "Konoha",
+    age: "8",
+  },
+  {
+    imageProfile: "https://pbs.twimg.com/profile_images/493416454104969216/2gt_nClw_400x400.jpeg",
+    name: "Goku",
+    city: "West City",
+    age: "11",
+  },
+];
