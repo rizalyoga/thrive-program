8bb288935be658539bacc86ed6415f508ff8eb17
rizalyoga <ach.rizal.yogaswara@gmail.com>
2022-04-15 16:35:54 +0700

    add modal component && set HP Bar

--- components/modal/Modal.jsx
+++ components/modal/Modal.jsx
@@ -6,13 +6,13 @@ import { useParams } from "react-router-dom";
 
 const Modal = ({ setIsOpen, idVillain }) => {
   const [dataVillain, setDataVillain] = useState([]);
-  const [playerHP, setPlayerHP] = useState(100);
-  const [villainHP, setVillainHP] = useState();
+  const [heroHP, setHeroHP] = useState(100);
+  const [villainHP, setVillainHP] = useState(100);
   const [loading, setLoading] = useState();
 
   const { nameCharacter } = useParams();
 
-  //Get Data Villain
+  //Get Data Villain Function
   useEffect(() => {
     getSelectedVillain(idVillain)
       .then((response) => setDataVillain(response))
@@ -25,9 +25,15 @@ const Modal = ({ setIsOpen, idVillain }) => {
     setIsOpen(false);
   };
 
-  // Start Battle
-  const startBattle = () => {
-    alert("Bertarungan dimulai");
+  // Start Battle Handler
+  const startBattle = (HPHero, HPVillain) => {
+    const payload = {
+      heroHP: HPHero,
+      villainHP: HPVillain,
+    };
+    console.log(payload);
+    setHeroHP((prev) => (prev > 0 ? prev - 10 : 0));
+    setVillainHP((prev) => (prev > 0 ? prev - 10 : prev === 0 ? 0 : null));
   };
 
   return (
@@ -36,7 +42,7 @@ const Modal = ({ setIsOpen, idVillain }) => {
       <div className="centered">
         <div className="modal" style={{ backgroundImage: `linear-gradient(#b3b3b3b2, #ffffffb1),url(https://i.pinimg.com/originals/33/9f/b7/339fb7afe67658a9019f3b5cfb84ec19.jpg)`, backgroundSize: "cover" }}>
           <div className="modalHeader">
-            <h5 className="heading">- Battle with {dataVillain[0]?.name.toUpperCase()} -</h5>
+            <h5 className="heading">- BATTLE WITH {dataVillain[0]?.name.toUpperCase()} -</h5>
           </div>
           <button className="closeBtn" onClick={closeModal}>
             <RiCloseLine style={{ marginBottom: "-3px" }} />
@@ -54,27 +60,28 @@ const Modal = ({ setIsOpen, idVillain }) => {
                 <h3>- {dataVillain[0]?.name} -</h3>
 
                 {/*  ----------------------------- HP Bar Villain -----------------------------  */}
-                <div className="progress" style={villainHP ? { width: `${villainHP}` } : { width: `${dataVillain[0]?.maxHP}` + "%" }}>
-                  <p className="statusHP">{villainHP ? `${villainHP}` : `${dataVillain[0]?.maxHP}`} %</p>
+                <div className="progress" style={{ maxWidth: `${dataVillain[0]?.maxHP}` + "%" }}>
+                  <p className="statusHP">{villainHP >= 0 ? `${villainHP}` : `${dataVillain[0]?.maxHP}`} %</p>
 
-                  <div className="villainHP progressBar" style={villainHP ? { width: `${villainHP}` } : { maxWidth: `${dataVillain[0]?.maxHP}` + "%" }}></div>
+                  <div className="villainHP progressBar" style={villainHP >= 0 ? { width: `${villainHP}` + "%" } : { width: `${dataVillain[0]?.maxHP}` + "%" }}></div>
                 </div>
 
+                {/* ------------------------------ Battle Status -----------------------------  */}
                 <div className="boxStatus">
                   <h2>Ready</h2>
                 </div>
 
                 {/*  ----------------------------- HP Bar Player -----------------------------  */}
                 <div className="progress">
-                  <p className="statusHP">{playerHP} %</p>
-                  <div className="playerHP progressBar" style={{ width: `${playerHP}` + "%" }}></div>
+                  <p className="statusHP">{heroHP} %</p>
+                  <div className="playerHP progressBar" style={{ width: `${heroHP}` + "%" }}></div>
                 </div>
                 <h3>- {nameCharacter} -</h3>
 
                 {/*  ----------------------------- Action Control -----------------------------  */}
                 <div className="modalActions">
                   <div className="actionsContainer">
-                    <button className="fightBtn" onClick={startBattle}>
+                    <button className="fightBtn" onClick={() => startBattle(heroHP, dataVillain[0].maxHP)}>
                       Fight
                     </button>
                     {/* <button className="cancelBtn" onClick={closeModal}>
